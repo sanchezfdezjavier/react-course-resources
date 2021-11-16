f809f80b93a1e8818baedc512a52efd1ee26c7c9
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-11-16 21:08:31 +0000

    Feat: 240

--- redux-test/src/store/index.js
+++ redux-test/src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore } from "redux";
 
-const conuterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const conuterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
@@ -11,12 +14,21 @@ const conuterReducer = (state = { counter: 0 }, action) => {
     console.log("action incrementbyx");
     return {
       counter: state.counter + action.value,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
