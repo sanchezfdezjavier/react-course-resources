df0d9525a9cb9dbadae689f0d6bed2894bb55d46
Javi Sanchez <javi@Javis-MacBook-Pro.local>
2021-11-21 20:49:01 +0000

    Feat(Authentication): 303

--- redux-v2/src/App.js
+++ redux-v2/src/App.js
@@ -1,12 +1,32 @@
-import { computeHeadingLevel } from "@testing-library/react";
-import { useSelector } from "react-redux";
+import { useSelector, useEffect, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
+import { uiActions } from "./store/ui-slice";
+
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-course-f7e10-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending data cart data failed!");
+      }
+
+      const responseData = await response.json();
+    };
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
