a0f69a6772b649d6de2b15a640019d78b23f972b
Javi Sanchez <javi@Javis-MacBook-Pro.local>
2021-11-27 15:00:40 +0000

    Feat: 349

---
+++ authentication/src/store/auth-context.js
@@ -0,0 +1,36 @@
+import { React, useState, createContext } from "react";
+
+const AuthContext = createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+  const userIsLoggedIn = !!token;
+
+  const loginHanlder = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHanlder,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
