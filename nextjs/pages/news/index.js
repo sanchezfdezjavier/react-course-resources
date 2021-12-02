f7c600ceae890e0067ca29d392b259c2dcbc6715
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-12-02 23:18:50 +0000

    Feat: dynamic routes 326

--- nextjs/pages/news/index.js
+++ nextjs/pages/news/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function HomePage() {
+export default function News() {
   return (
     <div>
-      <h1>Hello next</h1>
+      <h1>News page</h1>
     </div>
   );
 }
