c6240c21be52f0a41e91de8c7a882f969ef6eef7
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-12-08 19:48:27 +0000

    Feat: Adds more tests

--- testing/src/components/Greeting.js
+++ testing/src/components/Greeting.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
+
+import Output from './Output';
 
 export default function Greeting() {
+    const [test, setText] = useState(false);
+
+    const onClickHandler = () => {
+        setText(true)
+    }
     return (
         <div>
             <h2>Hello world!</h2>
-            <p>It's good to see you</p>
+            {!test && <Output>It's good to see you</Output>}
+            {test && <Output>Changed</Output>}
+            <button onClick={onClickHandler}>Change text</button>
         </div>
     )
 }
--- testing/src/components/Greeting.test.js
+++ testing/src/components/Greeting.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import Greeting from './Greeting'
 
 describe('Greeting component', () => {
@@ -15,6 +17,36 @@ describe('Greeting component', () => {
         const helloWorldElement = screen.getByText(/Hello world!/i);
         expect(helloWorldElement).toBeInTheDocument();
     })
+
+    test('renders by default Its good to see you', () => {
+        render(<Greeting/>)
+
+        const itsGoodElement = screen.getByText(/It's good to see you/i)
+        expect(itsGoodElement).toBeInTheDocument();
+    })
+
+    test('renders changed after button pressed', () => {
+        render(<Greeting/>)
+
+        const button = screen.getByRole('button');
+        userEvent.click(button)
+
+
+        const changedElement = screen.getByText(/Changed/i)
+        expect(changedElement).toBeInTheDocument();
+    })
+
+    test('not renders Its good to see you after clicking the button', () => {
+        render(<Greeting/>)
+
+        const button = screen.getByRole('button');
+        userEvent.click(button)
+
+
+        const changedElement = screen.queryByText(/It's good to see you/i)
+        expect(changedElement).toBeNull()
+    })
+    
 })
 
 
